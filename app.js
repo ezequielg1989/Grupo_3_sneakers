0bcf0b38fdcb250c8f34b7cd9ae011b39f61d4b6
ezequielg1989 <ezequiel.ganzero1@gmail.com>
2021-08-29 20:55:45 -0300

    agregando index y estilos

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static(path.join(__dirname, "./public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/index.html"));
 });
+
 app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/register.html"));
 });
