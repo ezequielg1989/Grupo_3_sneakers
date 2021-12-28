a9d9908492e377b410a5baaf5992fbf2fd25dabb
Axel Garrido <axel_g10@hotmail.com>
2021-12-28 20:23:00 -0300

    modificado el orden de los archivos

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const routesMain = require("./routes/main.routes");
+const routesAdmin = require("./routes/admin.routes");
+const indexRouter = require("./routes/index");
+
+const PORT = process.env.PORT || 3002;
+
+app.use(
+  session({
+    secret: "inicia session",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(methodOverride("_method"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+//app.use('/',routesMain);
+//app.use('/',routesAdmin);
+
+app.use((req, res, next) => {
+  res.status(404).render("not-found");
+});
+
+module.exports = app;
